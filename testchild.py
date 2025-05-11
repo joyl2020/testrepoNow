d2d59aa1fda480296cd3d5732c7e497993852b25
joyl2020 <60604589+joyl2020@users.noreply.github.com>
2025-05-11 15:48:59 +0200

    Update testchild.py from child again and again

--- testchild.py
+++ testchild.py
@@ -4,3 +4,6 @@ print("Inside the child repo.")
 
 #Changeds from a pull request
 print("This is a pull request from the child request.")
+
+#Display pull from the  child with the request again and again
+print("Print from the pull from the pull and time and time again.")
